1950ac4a6f5c6df0991d4c6838ba43107673950c
JacobWylie <jacobwylie@gmail.com>
2017-10-19 15:24:30 +0200

    components add and card

---
+++ react/hello-world/src/HobbyList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HobbyList extends Component {
+	render () {
+		return (
+			<ul>
+				<li>Sleeping</li>
+				<li>Eating</li>
+				<li>Pooping</li>
+			</ul>
+		)
+	}
+}
+
+export default HobbyList;

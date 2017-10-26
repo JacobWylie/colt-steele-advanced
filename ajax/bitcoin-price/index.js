df013106719d9ecfb3169d1c8986f5dbf4260521
JacobWylie <jacobwylie@gmail.com>
2017-10-26 14:53:28 +0200

    added comments

--- ajax/bitcoin-price/index.js
+++ ajax/bitcoin-price/index.js
@@ -3,6 +3,7 @@ const button = document.querySelector('button');
 const currency = document.querySelectorAll('.curr');
 
 function getPrice() {
+	// Get the value of the selected currency
 	let curr;
 	currency.forEach(function(currency) {
 		if(currency.checked) {
@@ -11,11 +12,13 @@ function getPrice() {
 		return curr;
 	})
 
+	// AJAX request for current Bitcoin price
 	const xhr = new XMLHttpRequest();
 
 	xhr.onreadystatechange = function() {
 		if(xhr.readyState === 4) {
 			let data = JSON.parse(xhr.responseText);
+			// Change the HTML text
 			price.innerHTML = `${data.bpi[curr].symbol} ${data.bpi[curr].rate}`;
 		}
 	}
@@ -24,10 +27,13 @@ function getPrice() {
 	xhr.send();
 }
 
+// Call function when button is clicked
 button.addEventListener('click', getPrice);
 
+// Call function when currency is changed
 currency.forEach(currency => {
 	currency.addEventListener("click", getPrice);
 })
 
+// Call the function on page load
 getPrice();

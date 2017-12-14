28de2a461c87572300929c18104e7a4ff418fb83
JacobWylie <jacobwylie@gmail.com>
2017-12-14 18:45:08 +0100

    proto exercise 2

--- OOP/prototypes/exercise.js
+++ OOP/prototypes/exercise.js
@@ -1,3 +1,25 @@
+/* 
+
+1 - Create a constructor function for a Person. Each person should have a firstName, lastName, 
+	favoriteColor, favoriteNumber)
+
+2 - Add a function on the Person.prototype called fullName that returns the firstName and lastName 
+	property of an object created by the Person constructor concatenated together.
+    
+Examples:    
+	var person = new Person("Elie", "Schoppik", "purple", 34)
+    person.fullName() // "Elie Schoppik"
+
+3 -  Add a property on the object created from the Person function called family which is an empty 
+	 array. This will involve you adding an additional line of code to your Person constructor.
+
+4 - Add a function on the Person.prototype called addToFamily which adds an object constructed from 
+	the Person constructor to the family array. To make sure that the object you are adding is an object 
+	construced from the Person constructor (HINT - take a look at the instanceof keyword). Make sure that 
+	your family array does not include duplicates! This method should return the length of the family array.
+
+*/
+
 function Person(firstName, lastName, favoriteColor, favoriteNumber) {
 	this.firstName = firstName;
 	this.lastName = lastName;

3ac2ebee464851dd9c5e4995a3adadaffa781d2e
JacobWylie <jacobwylie@gmail.com>
2017-12-20 18:42:28 +0100

    POST todo and update DOM

--- todo-api/public/app.js
+++ todo-api/public/app.js
@@ -1,7 +1,8 @@
 const list = document.querySelector('.list');
+const input = document.querySelector('#todoInput');
+const url = "http://localhost:3000/api/todos";
 
 function getTodos() {
-	const url = "http://localhost:3000/api/todos";
 	fetch(url)
 	.then(handleErrors)
 	.then(addTodos)
@@ -18,12 +19,72 @@ function addTodos(res) {
 	res.json()
 	.then( data => {
 		data.forEach(todo => {
-			const newTodo = document.createElement('li');
-			newTodo.classList.add('task');
-			newTodo.textContent = todo.name;
-			list.appendChild(newTodo);
+			addTodo(todo)
 		})
 	})
 }
 
+function addTodo(todo) {
+	const newTodo = document.createElement('li');
+	newTodo.classList.add('task');
+	newTodo.textContent = todo.name;
+	if(todo.completed) {
+		newTodo.classList.add('done');
+	}
+	list.appendChild(newTodo);
+}
+
+function newTodo(userInput) {
+	fetch(url, {
+		method: "POST",
+		headers: {
+			"Accept": "application/json, text/plain, */*",
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({name: userInput})
+	})
+	.then(handleErrors)
+	.then(res => res.json())
+	.then(todo => addTodo(todo))
+}
+
+input.addEventListener('keydown', e => {
+	if (e.keyCode == 13) {
+		newTodo(e.target.value);
+		e.target.value = "";
+	}
+}) 
+	
+
 getTodos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

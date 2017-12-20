2de6be33c4617079020a023d99915176106d8187
JacobWylie <jacobwylie@gmail.com>
2017-12-20 17:34:29 +0100

    make ajax call on db using fetch() and append data to site

--- todo-api/public/app.js
+++ todo-api/public/app.js
@@ -1,1 +1,29 @@
-console.log('coming from app.js')
+const list = document.querySelector('.list');
+
+function getTodos() {
+	const url = "http://localhost:3000/api/todos";
+	fetch(url)
+	.then(handleErrors)
+	.then(addTodos)
+}
+
+function handleErrors(res) {
+	if(!res.ok) {
+		throw Error(res.status);
+	}
+	return res;
+}
+
+function addTodos(res) {
+	res.json()
+	.then( data => {
+		data.forEach(todo => {
+			const newTodo = document.createElement('li');
+			newTodo.classList.add('task');
+			newTodo.textContent = todo.name;
+			list.appendChild(newTodo);
+		})
+	})
+}
+
+getTodos();

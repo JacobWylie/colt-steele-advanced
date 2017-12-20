a268239f7308199d18be738555534f79abe75b7b
JacobWylie <jacobwylie@gmail.com>
2017-12-20 14:17:15 +0100

    DELETE route and refactoring routes with helpers

---
+++ todo-api/helpers/todos.js
@@ -0,0 +1,54 @@
+const db = require('../models');
+
+exports.getTodos = function(req, res) {
+	db.Todo.find()
+	.then( todos => {
+		res.json(todos);
+	})
+	.catch( err => {
+		res.send(err);
+	})
+}
+
+exports.createTodo = function(req, res) {
+	db.Todo.create(req.body)
+	.then( newTodo => {
+		res.status(201).json(newTodo)
+	})
+	.catch( err => {
+		res.send(err)
+	})
+}
+
+exports.showTodo = function(req, res) {
+	db.Todo.findById(req.params.todoId)
+	.then( todo => {
+		res.json(todo)
+	})
+	.catch( err => {
+		res.send(err)
+	})
+}
+
+exports.updateTodo = function(req, res) {
+	db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+	// same as findByIdAndUpdate(req.params.todoId, req.body)
+	.then( todo => {
+		res.json(todo)
+	})
+	.catch( err => {
+		res.send(err)
+	})
+}
+
+exports.deleteTodo = function(req, res) {
+	db.Todo.remove({_id: req.params.todoId})
+	.then( () => {
+		res.json({message: `Todo ${req.params.todoId} has been deleted`})
+	})
+	.catch( err => {
+		res.send(err)
+	})
+}
+
+module.exports = exports;
--- todo-api/routes/todos.js
+++ todo-api/routes/todos.js
@@ -1,47 +1,40 @@
 const express = require('express'),
 	  router  = express.Router(),
-	  db      = require('../models');
+	  db      = require('../models'),
+	  helpers = require('../helpers/todos')
 
 
-router.get('/', (req, res) => {
-	db.Todo.find()
-	.then( todos => {
-		res.json(todos);
-	})
-	.catch( err => {
-		res.send(err);
-	})
-})
+router.route('/')
+	.get(helpers.getTodos)
+	.post(helpers.createTodo)
 
-router.post('/', (req, res) => {
-	db.Todo.create(req.body)
-	.then( newTodo => {
-		res.status(201).json(newTodo)
-	})
-	.catch( err => {
-		res.send(err)
-	})
-})
-
-router.get('/:todoId', (req, res) => {
-	db.Todo.findById(req.params.todoId)
-	.then( todo => {
-		res.json(todo)
-	})
-	.catch( err => {
-		res.send(err)
-	})
-})
-
-router.put('/:todoId', (req, res) => {
-	db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-	// same as findByIdAndUpdate(req.params.todoId, req.body)
-	.then( todo => {
-		res.json(todo)
-	})
-	.catch( err => {
-		res.send(err)
-	})
-})
+router.route('/:todoId')
+	.get(helpers.showTodo)
+	.put(helpers.updateTodo)
+	.delete(helpers.deleteTodo)
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

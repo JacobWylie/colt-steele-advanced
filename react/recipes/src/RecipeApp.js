7eb910226775896ceaea5716de194af5ce0a6035
JacobWylie <jacobwylie@gmail.com>
2018-01-30 14:41:58 +0100

    recipe app main app component built. imports and exports renamed to match recipe app and webpack compiles with no errors

---
+++ react/recipes/src/RecipeApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './RecipeApp.css'
+
+class RecipeApp extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Recipe App</h1>
+            </div>
+        );
+    }
+}
+
+export default RecipeApp;

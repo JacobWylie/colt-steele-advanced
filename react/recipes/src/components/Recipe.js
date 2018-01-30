b0f4b0ac10e28163157eba7307c06e23f03b5487
JacobWylie <jacobwylie@gmail.com>
2018-01-30 14:51:08 +0100

    reorganize file tree so components and css are in seperate folders. Create recipe component

---
+++ react/recipes/src/components/Recipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Recipe extends Component {
+	render() {
+		return (
+			<div>Recipes</div>
+		)
+	}
+}
+
+export default Recipe;
--- react/recipes/src/components/RecipeApp.js
+++ react/recipes/src/components/RecipeApp.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './RecipeApp.css'
+import '../styles/RecipeApp.css'
+import Recipe from './Recipe';
 
 class RecipeApp extends Component {
     render() {
         return (
             <div className="App">
                 <h1>Recipe App</h1>
+                <Recipe />
             </div>
         );
     }
